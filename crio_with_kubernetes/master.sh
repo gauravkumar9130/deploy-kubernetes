9776792450b364bf7a15484b56fe18861dddf0d8
gaurav <gauravkumar9130@gmail.com>
2021-03-04 15:53:44 +0530

    bug fix

--- crio_with_kubernetes/master.sh
+++ crio_with_kubernetes/master.sh
@@ -8,14 +8,20 @@ swapoff -a
 setenforce 0
 systemctl stop firewalld
 systemctl disable firewalld
-modprobe overlay
-modproble br_netfilter
-cat > /etc/sysctl.d/kubernetes.conf <<EOF
-net.bridge.bridge-nf-call-iptables=1
-net.bridge.bridge-nf-call-ip6tables=1
-net.ipv4.ip_forward=1
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
 EOF
-sysctl --system
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
 
 echo "********************************* Configure Kubernetes **************************************"
 cat > /etc/yum.repos.d/kubernetes.repo <<EOF
