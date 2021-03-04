7f084f07de5df354c6b03244f5663f840d762997
gaurav <gauravkumar9130@gmail.com>
2021-03-04 14:24:46 +0530

    change 1

--- crio_with_kubernetes/master.sh
+++ crio_with_kubernetes/master.sh
@@ -12,7 +12,7 @@ modprobe overlay
 modproble br_netfilter
 cat > /etc/sysctl.d/kubernetes.conf <<EOF
 net.bridge.bridge-nf-call-iptables=1
-net.bridge.bridge-nfs-call-ip6tables=1
+net.bridge.bridge-nf-call-ip6tables=1
 net.ipv4.ip_forward=1
 EOF
 sysctl --system
