c24c48c3749319e23af6e04970f990eb0245c55a
gaurav <gauravkumar9130@gmail.com>
2021-03-04 16:10:29 +0530

    bug fixes 2

--- crio_with_kubernetes/worker1.sh
+++ crio_with_kubernetes/worker1.sh
@@ -33,6 +33,8 @@ repo_gpgcheck=1
 enabled=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg https://packages.cloud.google.com/yum/doc/yum-key.gpg
 EOF
+yum clean all
+yum repolist all
 yum install kubectl kubelet kubeadm -y
 systemctl start kubelet
 systemctl enable kubelet
