5d37ba696d20ece14ab6eeaec013bc976e57b8b3
gauravkumar9130 <55126211+gauravkumar9130@users.noreply.github.com>
2022-09-22 17:07:06 +0530

    Update worker2.sh

--- crio_with_kubernetes/worker2.sh
+++ crio_with_kubernetes/worker2.sh
@@ -25,15 +25,15 @@ EOF
 sudo sysctl --system
 
 echo "********************************* Configure Kubernetes **************************************"
-cat > /etc/yum.repos.d/kubernetes.repo <<EOF
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
-name=kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-gpgcheck=0
-repo_gpgcheck=0
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
-gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg https://packages.cloud.google.com/yum/doc/yum-key.gpg
+gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
+
 yum clean all -y
 yum repolist all -y
 yum install kubectl kubelet kubeadm -y
